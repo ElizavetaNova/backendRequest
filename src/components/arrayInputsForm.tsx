1019a2458ea6e90fd71318fa006af05d67c3f022
Elizaveta <liza_fahrut@mail.ru>
2022-07-25 12:44:11 +1000

    Попыталась вынести в отдельный компонент вывод input для формы создания фильма

--- src/components/arrayInputsForm.tsx
+++ src/components/arrayInputsForm.tsx
@@ -1,3 +1,5 @@
+import { useForm } from 'react-hook-form';
+import { CreateMovieDto } from '../interfaces/createMovieDto';
 import { FormInput } from '../interfaces/formInput';
 
 export const formInputs: FormInput[] = [
@@ -45,3 +47,65 @@ export const formInputs: FormInput[] = [
         },
     },
 ];
+
+export function FormInputsDisplay () {
+    const { register, formState: { errors } } = useForm<CreateMovieDto>({
+        defaultValues: {},
+        reValidateMode: 'onChange',
+        mode: 'onBlur',
+    });
+    return (
+        <div>
+            {
+                formInputs.map((item) => {
+                    switch (item.type) {
+                        case undefined:
+                            return <>
+                                <input
+                                    className={'create-movie-input'}
+                                    placeholder={item.label}
+                                    {...register(item.name, item.validation)}
+                                />
+                                {
+                                    errors[item.name]?.message &&
+                                    <p className="error">
+                                        {errors[item.name]?.message}
+                                    </p>
+                                }
+                            </>;
+                        case 'textarea':
+                            return (
+                                <>
+                                    <textarea
+                                        className={'create-movie-input'}
+                                        placeholder={errors[item.name]?.message ?? item.label}
+                                        {...register(item.name, item.validation)}
+                                    />
+                                    {
+                                        errors[item.name]?.message &&
+                                        <p className="error">
+                                            {errors[item.name]?.message}
+                                        </p>
+                                    }
+                                </>);
+                        case 'date':
+                            return (
+                                <>
+                                    <input
+                                        type={'date'}
+                                        className={'create-movie-input'}
+                                        {...register(item.name, item.validation)}
+                                    />
+                                    {
+                                        errors[item.name]?.message &&
+                                        <p className="error">
+                                            {errors[item.name]?.message}
+                                        </p>
+                                    }
+                                </>
+                            );
+                    }
+                })
+            }
+        </div>
+)}
--- src/components/createMovies/createForm.tsx
+++ src/components/createMovies/createForm.tsx
@@ -2,7 +2,7 @@ import './createForm.scss';
 import { create } from '../../requests/requests';
 import { CreateMovieDto } from '../../interfaces/createMovieDto';
 import { useForm } from 'react-hook-form';
-import { formInputs } from '../arrayInputsForm';
+import { FormInputsDisplay } from '../arrayInputsForm';
 
 interface CreateMovieParams {
     hideForm(showForm: boolean): void;
@@ -19,7 +19,7 @@ export const Form = (props: CreateMovieParams) => {
         hideForm();
     }
 
-    const { register, formState: { errors }, handleSubmit } = useForm<CreateMovieDto>({
+    const { handleSubmit } = useForm<CreateMovieDto>({
         defaultValues: {},
         reValidateMode: 'onChange',
         mode: 'onBlur',
@@ -29,57 +29,10 @@ export const Form = (props: CreateMovieParams) => {
         <form className={'add-movie__form'}
             onSubmit={handleSubmit(onSubmit)}
         >
-            {
-                formInputs.map((item) => {
-                    switch (item.type) {
-                    case undefined:
-                        return <>
-                            <input
-                                className={'create-movie-input'}
-                                placeholder={item.label}
-                                {...register(item.name, item.validation)}
-                            />
-                            {
-                                errors[item.name]?.message &&
-                                <p className="error">
-                                    {errors[item.name]?.message}
-                                </p>
-                            }
-                        </>;
-                    case 'textarea':
-                        return (
-                            <>
-                                <textarea
-                                    className={'create-movie-input'}
-                                    placeholder={errors[item.name]?.message ?? item.label}
-                                    {...register(item.name, item.validation)}
-                                />
-                                {
-                                    errors[item.name]?.message &&
-                                    <p className="error">
-                                        {errors[item.name]?.message}
-                                    </p>
-                                }
-                            </>);
-                    case 'date':
-                        return (
-                            <>
-                                <input
-                                    type={'date'}
-                                    className={'create-movie-input'}
-                                    {...register(item.name, item.validation)}
-                                />
-                                {
-                                    errors[item.name]?.message &&
-                                    <p className="error">
-                                        {errors[item.name]?.message}
-                                    </p>
-                                }
-                            </>
-                        );
-                    }
-                })
-            }
+            
+            <FormInputsDisplay />
+                    
+            
             <button className={'primary-button'}>
                 Submit
             </button>
